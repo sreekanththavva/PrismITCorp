77c3a03a78fd351dda11a732a8c491a590e48cae
Sreekanth Reddy <sreekanththavva@gmail.com>
2019-01-23 18:05:55 -0500

    Trying to upload

---
+++ PrismITCorp/src/com/primsmitcorp/java/sreekanth/jan24th/HelloWorld.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.primsmitcorp.java.sreekanth.jan24th;
+
+/**
+ * @author sreekanthreddy
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello World...!");
+
+	}
+
+}
---
+++ PrismITCorp/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author sreekanthreddy
+ *
+ */
+module PrismITCorp {
+}
