7591b60b624a92fe85e4a6ea2f3a86410bf14c08
Sreekanth Reddy <sreekanththavva@gmail.com>
2019-01-23 21:22:34 -0500

    Do while loop simple Example

---
+++ PrismITCorp/src/com/primsmitcorp/java/sreekanth/jan24th/DoWhileLoop.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.primsmitcorp.java.sreekanth.jan24th;
+
+/**
+ * @author sreekanthreddy
+ *
+ */
+public class DoWhileLoop {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		int num = 0;
+
+		do {
+			System.err.print(" " + num);
+			num++;
+		} while (num <= 20);
+	}
+
+}
