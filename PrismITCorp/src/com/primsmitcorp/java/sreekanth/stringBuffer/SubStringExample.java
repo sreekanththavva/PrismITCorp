2c2c0b74a106842f1c7c66bd684cc90275278899
Sreekanth Reddy <sreekanththavva@gmail.com>
2019-01-27 07:52:53 -0500

    substring

--- PrismITCorp/src/com/primsmitcorp/java/sreekanth/stringBuffer/SubStringExample.java
+++ PrismITCorp/src/com/primsmitcorp/java/sreekanth/stringBuffer/SubStringExample.java
@@ -15,11 +15,10 @@ public class SubStringExample {
 	public static void main(String[] args) {
 		StringBuffer stringBuffer = new StringBuffer("Before SubString printing");
 		System.out.println(stringBuffer);
-		
+		// Substring starts form the letter with new string
 		System.out.println(stringBuffer.substring(7));
-		System.out.println(stringBuffer.substring(7,16));
+		System.out.println(stringBuffer.substring(7, 16));
 
-		
 	}
 
 }
